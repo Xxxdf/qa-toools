146525c4ef43cf11174272dad673054725c01d2c
Haoyu Fang <haoyufang@moonton.com>
2023-03-10 17:34:49 +0800

    修改绘制的方法

--- app_resource/reporter.py
+++ app_resource/reporter.py
@@ -7,9 +7,8 @@ import os
 import sys
 
 from pyecharts import options as opts
-from pyecharts.charts import Line, Pie, Grid, Scatter
+from pyecharts.charts import Line, Pie, Grid
 from pyecharts.render import make_snapshot
-from pyecharts.faker import Faker
 import matplotlib.pyplot as plt
 from snapshot_phantomjs import snapshot
 from pyecharts.globals import CurrentConfig
@@ -23,6 +22,9 @@ from LarkBot import LarkBot
 CurrentConfig.ONLINE_HOST = "https://cdn.jsdelivr.net/npm/echarts@latest/dist/"
 plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
+
+PACK_PNG = "Pack.png"       # 包体资源变化趋势
+
 
 
 def draw_line(date, apk_pack, ipa_pack):
@@ -39,7 +41,7 @@ def draw_line(date, apk_pack, ipa_pack):
                          yaxis_opts=opts.AxisOpts(min_=int(mean*0.8)))
     )
 
-    make_snapshot(snapshot, c.render(), "Pack.png", pixel_ratio=2)
+    make_snapshot(snapshot, c.render(), PACK_PNG, pixel_ratio=2)
 
 
 def draw_multi_line(date, data, maker, sort):
@@ -66,29 +68,27 @@ def draw_multi_line(date, data, maker, sort):
 
     # 给figure加横轴的名称，范围在[0,1]之间
     plt.figtext(0.5, 0.05, '日期', fontsize=20)
-    # plt.figtext(0.05, 0.5, 'DrugReports', va='center', rotation='vertical', fontsize=15)
 
     # figure的标题
     plt.suptitle(f"{sort}首包各资源变化情况", fontsize=25, weight="bold")
 
-    plt.savefig(f"{sort}.png")
+    plt.savefig(f"{sort}_line.png")
     plt.show()
 
 
-
-def draw_pie(data, title, file_name):
-    pack_size = data["size"]
-    data_ = get_percent(resource=data["resource"], pack=pack_size[0])
+def draw_pie(data, kind):
+    pack_ = data[-1]
+    resource_ = get_percent(resource=data[:-1], pack=pack_)
 
     c = (
         Pie()
         .add(
             "",
-            data_,
+            resource_,
             radius=["40%", "75%"],
         )
         .set_global_opts(
-            title_opts=opts.TitleOpts(title=title,
+            title_opts=opts.TitleOpts(title=f"{kind}首包资源占比",
                                       title_textstyle_opts=opts.TextStyleOpts(font_family="Microsoft YaHei")),
             legend_opts=opts.LegendOpts(orient="vertical", pos_top="15%", pos_left="2%",
                                         textstyle_opts=opts.TextStyleOpts(font_family="Microsoft YaHei"))
@@ -97,7 +97,7 @@ def draw_pie(data, title, file_name):
                                                    font_family="FangSong_GB2312"))
     )
 
-    make_snapshot(snapshot, c.render(), file_name, pixel_ratio=2)
+    make_snapshot(snapshot, c.render(), f"{kind}_pie.png", pixel_ratio=2)
 
 
 def get_percent(resource, pack):
@@ -119,31 +119,11 @@ class DailyReporter(LarkBot):
     def __init__(self, apk_size, ipa_size):
         super(DailyReporter, self).__init__()
 
-        self.apk_key = None
-        self.ipa_key = None
+        self.apk_size = apk_size
+        self.ipa_size = ipa_size
 
-        self.apk_size = self._get_size("mlbb_trunk.apk")
-        self.ipa_size = self._get_size("mlbb_trunk.ipa")
-
-    @staticmethod
-    def _get_size(path):
-        size = os.path.getsize(path)
-        actual = size/(1024*1024)
-        return round(actual, 2)
-
-    def prepare(self):
-        # apk_change = self._build(self.apk_size)
-        # ipa_change = self._build(self.ipa_size)
-
-        self.apk_key = self.get_image_key("Android.png")
-        self.ipa_key = self.get_image_key("IOS.png")
-
-        pack_key = self.get_image_key("Pack.png")
-
-        return self.init_card(key=pack_key)
-
-    def init_card(self, key):
-        card = {
+    def init_card(self, pack_key, apk_line, ipa_line, apk_pie, ipa_pie):
+        c = {
             "elements": [
                 {
                     "tag": "column_set",
@@ -169,14 +149,14 @@ class DailyReporter(LarkBot):
                                             "is_short": True,
                                             "text": {
                                                 "tag": "lark_md",
-                                                "content": f"**Androi包体大小：**\n**{self.apk_size}MB**"
+                                                "content": f"**Android包体大小：\n{self.apk_size}MB**"
                                             }
                                         },
                                         {
                                             "is_short": True,
                                             "text": {
                                                 "tag": "lark_md",
-                                                "content": f"**IOS包体大小：**\n**{self.ipa_size}MB**"
+                                                "content": f"**IOS包体大小：\n{self.ipa_size}**"
                                             }
                                         }
                                     ]
@@ -187,7 +167,7 @@ class DailyReporter(LarkBot):
                 },
                 {
                     "tag": "img",
-                    "img_key": key,
+                    "img_key": pack_key,
                     "alt": {
                         "tag": "plain_text",
                         "content": ""
@@ -203,28 +183,102 @@ class DailyReporter(LarkBot):
                 },
                 {
                     "tag": "markdown",
-                    "content": "**细分资源变化**",
+                    "content": "**细分资源占比**",
                     "text_align": "center"
                 },
                 {
-                    "tag": "img",
-                    "img_key": self.apk_key,
-                    "alt": {
-                        "tag": "plain_text",
-                        "content": ""
-                    },
-                    "mode": "fit_horizontal",
-                    "preview": True
+                    "tag": "column_set",
+                    "flex_mode": "none",
+                    "background_style": "grey",
+                    "columns": [
+                        {
+                            "tag": "column",
+                            "width": "weighted",
+                            "weight": 1,
+                            "vertical_align": "top",
+                            "elements": [
+                                {
+                                    "tag": "img",
+                                    "img_key": apk_pie,
+                                    "alt": {
+                                        "tag": "plain_text",
+                                        "content": ""
+                                    },
+                                    "mode": "fit_horizontal",
+                                    "preview": True
+                                }
+                            ]
+                        },
+                        {
+                            "tag": "column",
+                            "width": "weighted",
+                            "weight": 1,
+                            "vertical_align": "top",
+                            "elements": [
+                                {
+                                    "tag": "img",
+                                    "img_key": ipa_pie,
+                                    "alt": {
+                                        "tag": "plain_text",
+                                        "content": ""
+                                    },
+                                    "mode": "fit_horizontal",
+                                    "preview": True
+                                }
+                            ]
+                        }
+                    ]
                 },
                 {
-                    "tag": "img",
-                    "img_key": self.ipa_key,
-                    "alt": {
-                        "tag": "plain_text",
-                        "content": ""
-                    },
-                    "mode": "fit_horizontal",
-                    "preview": True
+                    "tag": "hr"
+                },
+                {
+                    "tag": "markdown",
+                    "content": "**细分资源变化情况**\n",
+                    "text_align": "center"
+                },
+                {
+                    "tag": "column_set",
+                    "flex_mode": "none",
+                    "background_style": "grey",
+                    "columns": [
+                        {
+                            "tag": "column",
+                            "width": "weighted",
+                            "weight": 1,
+                            "vertical_align": "top",
+                            "elements": [
+                                {
+                                    "tag": "img",
+                                    "img_key": apk_line,
+                                    "alt": {
+                                        "tag": "plain_text",
+                                        "content": ""
+                                    },
+                                    "mode": "fit_horizontal",
+                                    "preview": True
+                                }
+                            ]
+                        },
+                        {
+                            "tag": "column",
+                            "width": "weighted",
+                            "weight": 1,
+                            "vertical_align": "top",
+                            "elements": [
+                                {
+                                    "tag": "img",
+                                    "img_key": ipa_line,
+                                    "alt": {
+                                        "tag": "plain_text",
+                                        "content": ""
+                                    },
+                                    "mode": "fit_horizontal",
+                                    "preview": True
+                                }
+                            ]
+                        }
+                    ]
                 }
             ],
             "header": {
@@ -235,7 +289,112 @@ class DailyReporter(LarkBot):
                 }
             }
         }
-        return card
+        return c
+
+    def prepare(self):
+        pack_key = self.get_image_key(PACK_PNG)
+        apk_line = self.get_image_key("Android_line.png")
+        ipa_line = self.get_image_key("IOS_line.png")
+        apk_pie = self.get_image_key("Android_pie.png")
+        ipa_pie = self.get_image_key("IOS_pie.png")
+
+        c = self.init_card(pack_key=pack_key, apk_line=apk_line, ipa_line=ipa_line, apk_pie=apk_pie, ipa_pie=ipa_pie)
+        return c
+
+    # def init_card(self, key):
+    #     card = {
+    #         "elements": [
+    #             {
+    #                 "tag": "column_set",
+    #                 "flex_mode": "none",
+    #                 "background_style": "default",
+    #                 "columns": []
+    #             },
+    #             {
+    #                 "tag": "column_set",
+    #                 "flex_mode": "none",
+    #                 "background_style": "default",
+    #                 "columns": [
+    #                     {
+    #                         "tag": "column",
+    #                         "width": "weighted",
+    #                         "weight": 1,
+    #                         "vertical_align": "top",
+    #                         "elements": [
+    #                             {
+    #                                 "tag": "div",
+    #                                 "fields": [
+    #                                     {
+    #                                         "is_short": True,
+    #                                         "text": {
+    #                                             "tag": "lark_md",
+    #                                             "content": f"**Androi包体大小：**\n**{self.apk_size}MB**"
+    #                                         }
+    #                                     },
+    #                                     {
+    #                                         "is_short": True,
+    #                                         "text": {
+    #                                             "tag": "lark_md",
+    #                                             "content": f"**IOS包体大小：**\n**{self.ipa_size}MB**"
+    #                                         }
+    #                                     }
+    #                                 ]
+    #                             }
+    #                         ]
+    #                     }
+    #                 ]
+    #             },
+    #             {
+    #                 "tag": "img",
+    #                 "img_key": key,
+    #                 "alt": {
+    #                     "tag": "plain_text",
+    #                     "content": ""
+    #                 },
+    #                 "mode": "fit_horizontal",
+    #                 "preview": True
+    #             },
+    #             {
+    #                 "tag": "hr"
+    #             },
+    #             {
+    #                 "tag": "hr"
+    #             },
+    #             {
+    #                 "tag": "markdown",
+    #                 "content": "**细分资源变化**",
+    #                 "text_align": "center"
+    #             },
+    #             {
+    #                 "tag": "img",
+    #                 "img_key": self.apk_key,
+    #                 "alt": {
+    #                     "tag": "plain_text",
+    #                     "content": ""
+    #                 },
+    #                 "mode": "fit_horizontal",
+    #                 "preview": True
+    #             },
+    #             {
+    #                 "tag": "img",
+    #                 "img_key": self.ipa_key,
+    #                 "alt": {
+    #                     "tag": "plain_text",
+    #                     "content": ""
+    #                 },
+    #                 "mode": "fit_horizontal",
+    #                 "preview": True
+    #             }
+    #         ],
+    #         "header": {
+    #             "template": "turquoise",
+    #             "title": {
+    #                 "content": "首包体积变化情况",
+    #                 "tag": "plain_text"
+    #             }
+    #         }
+    #     }
+    #     return card
 
     def send_2_me(self, card):
         self.send_message(type_="email", id_="haoyufang@moonton.com", msg_type="interactive", content=card)
@@ -258,24 +417,21 @@ if __name__ == "__main__":
     operator = SQLOperator(table="Inpack")
 
     # last_data = operator.fetch_daily_data()
-    # # 绘制Android的
-    # draw_pie(data=last_data["apk"], title="Android首包资源占比", file_name="apk_daily.png")
-    # # 绘制IOS的
-    # draw_pie(data=last_data["ipa"], title="IOS首包资源占比", file_name="ipa_daily.png")
-    #
-    # bot = DailyReporter(apk_size=last_data["apk"]["size"], ipa_size=last_data["ipa"]["size"])
-    # card = bot.prepare()
-    # bot.send_2_me(card)
 
     last_days = operator.fetch_last_times()
     array_apk = np.array(last_days["apk"])
     array_ipa = np.array(last_days["ipa"])
+    last_apk = array_apk[-1]
+    last_ipa = array_ipa[-1]
+
+    draw_pie(data=last_apk, kind="Android")
+    draw_pie(data=last_ipa, kind="IOS")
 
     draw_multi_line(date=last_days["date"], data=array_apk, maker="8", sort="Android")
     draw_multi_line(date=last_days["date"], data=array_ipa, maker="d", sort="IOS")
 
     draw_line(date=last_days["date"], apk_pack=array_apk[:, -1], ipa_pack=array_ipa[:, -1])
 
-    bot = DailyReporter("", "")
+    bot = DailyReporter(last_apk[-1], last_ipa[-1])
     card = bot.prepare()
     bot.send_2_me(card)
