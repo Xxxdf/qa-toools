e3494cf494acadc3b731c5390b0be2a2bcfb6a4b
Haoyu Fang <haoyufang@moonton.com>
2023-03-14 11:22:34 +0800

    临时增加streamlit相关的代码测试

---
+++ ParseConfig/test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author  : Fang Haoyu
+# @Time    : 2022/12/19 14:13
+# @File    : test.py 
+# @Desc    : 根据值寻找对应的内容
+from operator import itemgetter
+
+def my_func(**kwargs):
+    print(kwargs)
+
+
+my_func()
