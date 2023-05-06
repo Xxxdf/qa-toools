10eb3e381ccc353c390cc29a3a4c3935db60052e
Haoyu Fang <haoyufang@moonton.com>
2023-05-06 15:58:46 +0800

    增加封板阶段的逻辑

--- scan_log/scan_log.py
+++ scan_log/scan_log.py
@@ -5,9 +5,7 @@
 # @Desc    : 扫描svn的日志
 
 
-import subprocess
 import datetime
-import re
 import json
 import os
 import sys
@@ -19,146 +17,92 @@ import pandas as pd
 sys.path.append(os.path.abspath(os.path.dirname(os.getcwd())))
 from utils import style_df
 from LarkBot import LarkBot
+from scanner import LegalityScanner, OnlineScanner
 
 
 with open("G.json", "r") as load_f:
     user_info = json.load(load_f)
 
-invalid_user = set()
-
 
 class Controller(object):
 
-    def __init__(self):
+    def __init__(self, dt_range, branches, scanner, excel_name="提交规范检查.xlsx"):
 
-        self.writer = pd.ExcelWriter("提交规范检查.xlsx")
+        self.start, self.end = dt_range
+        self.excel_name = excel_name
+        self.writer = pd.ExcelWriter(excel_name)
+        self.branches = branches
+        self.scanner = scanner
 
-    def run(self):
-        for branch in all_branches:
-            s = Scanner(branch=branch)
-            self._write_2_excel(df=s.scan())
+        self.user_info = user_info
+        self.illegal_user = set()
 
-        self._save()
+    def run(self, title, content):
+        # 结果写入excel
+        self._df_2_excel()
+        user_email = self._format_email()
 
-    def _save(self):
-        with open("G.json", "w") as f:
-            global user_info
-            json.dump(user_info, f)
+        excel_token = bot.import_excel(self.excel_name)
+        card = bot.init_msg_card(emails=user_email, start=self.start, end=self.end, title=title,
+                                 content=content, token=excel_token)
+        send_with_webhook(card)
+
+    def _df_2_excel(self):
+        """把df写入excel"""
+        for branch in self.branches:
+            s = self.scanner(branch=branch, start=self.start, end=self.end)
+            df = s.scan()
+
+            if df is None:
+                continue
+
+            df["提交人"] = df["提交人"].map(self._get_user)
+            self._write_2_excel(df)
 
         self.writer.close()
 
+    def _get_user(self, author):
+        """把user_id转化为用户名"""
+        try:
+            user_name, email = user_info[author]
+        except KeyError:
+            user_name, email = self._get_email_with_bot(author)   # 这里的detail包含用户名和邮箱地址
+            user_info[author] = (user_name, email)
+        self.illegal_user.add(email)
+        return user_name
+
+    @staticmethod
+    def _get_email_with_bot(user_id):
+        """
+        通过飞书机器人，获取用户名和邮箱
+        :param user_id:  对应用户的user_id
+        :return:  用户名、邮箱
+        """
+        resp = bot.get_user_info(user_id)
+        try:
+            user_detail = resp["data"]["user"]
+        except KeyError:
+            return user_id, ""
+        else:
+            name, email = user_detail.get("name"), user_detail.get("enterprise_email")
+
+            # 部分外包同学可能调api找不到邮箱
+            if email is None:
+                email = f"{user_id}@moonton.com"
+            pos = name.find("(")
+            if pos == -1:
+                return name, email
+            return name[:pos], email
+
+    def _format_email(self):
+        """把email格式化为飞书需要的形式"""
+        qa_list = [f"<at email={email}></at>" for email in self.illegal_user]
+        return "  ".join(qa_list) if qa_list else None
+
     def _write_2_excel(self, df):
-        if df is None:
-            return
+        """结果写入"""
         df.to_excel(self.writer, sheet_name="Sheet1", index=False, engine="xlsxwriter")
         style_df(df, writer_obj=self.writer, sheet_name="Sheet1")
-
-
-class Scanner(object):
-    def __init__(self, branch):
-        self.start = self._format_date(start)
-        self.end = self._format_date(end)
-        self.branch = branch
-
-    def scan(self):
-        logs = self._get_log()
-        illegal = self._scan(logs[1:-1])
-
-        # 如果都是合规提交
-        if not illegal:
-            return
-
-        df = pd.DataFrame(illegal, columns=["提交人", "提交时间", "提交日志"])
-        df["提交分支"] = self.branch
-        return df
-
-    def _scan(self, logs):
-        illegal = list()
-
-        for i, log in enumerate(logs):
-            if not log:
-                continue
-
-            # 提交信息相关
-            info = re.findall(r".*\|.*\|.*\|.*", log)[0]
-            _, committer, time_, _ = info.split("|")
-            author = committer.strip()
-
-            # 找不到单号且提交人不是builder
-            if not self._with_url(log) and author != "builder":
-                log_ = self._format_log(log.replace(info, ""))
-                user_name, user_email = self._get_author_name(author)
-                illegal.append([user_name, self._get_commit_time(time_), log_])
-
-                global invalid_user
-                if user_email:
-                    invalid_user.add(user_email)
-        return illegal
-
-    def _get_log(self):
-        """
-        获取指定日志
-        :return: List[str]
-        """
-        url = f"https://192.168.40.221:8833/svn/mlproj2017/branches/{self.branch}"
-        cmd = f"svn log -r {self.start}:{self.end} {url}"
-        cmd_return = subprocess.run(cmd, encoding="UTF-8", stdout=subprocess.PIPE, shell=True)
-        return cmd_return.stdout.split("------------------------------------------------------------------------")
-
-    @staticmethod
-    def _get_author_name(author):
-        """根据用户id，获取用户名和邮箱"""
-        global user_info
-        try:
-            return user_info[author]
-        except KeyError:
-            detail = get_user(author)   # 这里的detail包含用户名和邮箱地址
-            user_info[author] = detail
-            return detail
-
-    @staticmethod
-    def _get_commit_time(time_):
-        pos = time_.find("+")
-        return f"# {time_[:pos-1]}"
-
-    @staticmethod
-    def _with_url(log):
-        """提交中是否关联了飞书单"""
-        pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-        return True if re.findall(pattern, log) else False
-
-    @staticmethod
-    def _format_log(log):
-        return log.replace("\n", "")
-
-    @staticmethod
-    def _format_date(dt):
-        """将dt格式化为SVN log所需要的形式"""
-        return dt.strftime('{"%Y-%m-%d %H:%M:%S +0800"}')
-
-
-def get_user(user_id):
-    """
-    根据用户ID返回用户名名
-    :param user_id:
-    :return:
-    """
-
-    resp = bot.get_user_info(user_id)
-    try:
-        user_detail = resp["data"]["user"]
-    except KeyError:
-        return user_id, ""
-    else:
-        name, email = user_detail.get("name"), user_detail.get("enterprise_email")
-
-        # 部分外包同学可能调api找不到邮箱
-        if email is None:
-            email = f"{user_id}@moonton.com"
-        pos = name.find("(")
-        if pos == -1:
-            return name, email
-        return name[:pos], email
 
 
 # 当日的年份、月份
@@ -167,39 +111,39 @@ year, month, day = today.year, today.month, today.day
 
 
 class SendBot(LarkBot):
-    # 上层目录
-    parent_token = "fldcnLtm9g0SWcD658DJRvYmlrb"
     # 目标文件的名字
     target_folder = f"{year}.{month}"
 
-    def send(self):
-        user_email = self._format_email()
-        if user_email is None:
-            return
+    # 上层目录
+    parent_token = "fldcnLtm9g0SWcD658DJRvYmlrb"
 
+    def import_excel(self, excel_name):
+        """
+        把excel文件上传到飞书的云空间中
+        :param excel_name:  excel名
+        :return:
+        """
         result_folder = self.get_folder_token()
         temp_folder = self.create_folder(folder_name="temp", folder_token=self.parent_token)
 
-        ticket = self.upload_then_import(folder_token=temp_folder, excel_name="提交规范检查.xlsx",
+        ticket = self.upload_then_import(folder_token=temp_folder, excel_name=excel_name,
                                          result_token=result_folder)
         time.sleep(5)
         token = self.get_import_result(ticket)
-        card = self._init_msg_card(emails=user_email, token=token)
-
-        # self.send_message(type_="chat_id", id_="oc_3c06136bc6677d050af3c7831fca2efc", msg_type="interactive",
-        #                   content=card)
-
         self.delete_folder(temp_folder)
+        return token
 
-        return card
+    def init_msg_card(self, emails, token, title, start, end, content):
+        """
 
-    @staticmethod
-    def _format_email():
-        global invalid_user
-        qa_list = [f"<at email={email}></at>" for email in invalid_user]
-        return "  ".join(qa_list) if qa_list else None
-
-    def _init_msg_card(self, emails, token):
+        :param emails:  需要被@同学的email（已经处理为飞书api需要的形式）
+        :param token:   对应文件的token——用于拼接成url
+        :param title:   消息卡片的标题
+        :param start:   开始时间
+        :param end:     结束时间 （这个两个都是和scanner的一致）
+        :param content: 中文说明
+        :return:
+        """
         card = {
             "config": {
                 "wide_screen_mode": True
@@ -207,7 +151,7 @@ class SendBot(LarkBot):
             "elements": [
                 {
                     "tag": "markdown",
-                    "content": f"以下提交提交疑似为不规范提交\n\n请相关同学确认：{emails}"
+                    "content": f"{content}\n\n请相关同学确认：{emails}"
                 },
                 {
                     "actions": [
@@ -232,7 +176,7 @@ class SendBot(LarkBot):
             "header": {
                 "template": "yellow",
                 "title": {
-                    "content": f"提交规范检查（{self._format_date(start)}~{self._format_date(end)}）",
+                    "content": f"{title}（{self._format_date(start)}~{self._format_date(end)}）",
                     "tag": "plain_text"
                 }
             }
@@ -245,35 +189,61 @@ class SendBot(LarkBot):
         return dt.strftime("%m-%d %H:%M")
 
 
-def send_with_webhook(card, url):
+class DateTimeGenerator(object):
+
+    def __init__(self):
+        now = datetime.datetime.now()
+
+        # 今天的0点0分
+        self.zero = now - datetime.timedelta(hours=now.hour, minutes=now.minute, seconds=now.second,
+                                             microseconds=now.microsecond)
+
+    def illegal_dt_range(self):
+        """提交规范检查的"""
+        end = self.zero + datetime.timedelta(hours=20)
+        start = end - datetime.timedelta(days=1)
+
+        return start, end
+
+    def online_dt_range(self):
+        """提交规范检查的"""
+        end = self.zero + datetime.timedelta(hours=20)
+        start = end - datetime.timedelta(days=2)
+
+        return start, end
+
+
+def send_with_webhook(card):
+    url = "https://open.feishu.cn/open-apis/bot/v2/hook/13fc83d2-7f3e-4ce3-8db5-747bb3fe6046"   # 项目大群
+    # url = "https://open.feishu.cn/open-apis/bot/v2/hook/c3daa543-cd29-495f-bac3-bd9ab5329fb1"     # 调试群
     body = json.dumps({"msg_type": "interactive", "card": card})
-    res = requests.post(url=url, data=body, headers={"Content-Type":"application/json"})
+    res = requests.post(url=url, data=body, headers={"Content-Type": "application/json"})
     print(res.text)
 
 
 if __name__ == "__main__":
     bot = SendBot()
-    url = "https://open.feishu.cn/open-apis/bot/v2/hook/13fc83d2-7f3e-4ce3-8db5-747bb3fe6046"   # 项目大群
+    generator = DateTimeGenerator()
 
-    now = datetime.datetime.now()
-    zero = now - datetime.timedelta(hours=now.hour, minutes=now.minute, seconds=now.second, microseconds=now.microsecond)
+    type_ = sys.argv[1]
 
-    end = zero + datetime.timedelta(hours=20)
-    start = end - datetime.timedelta(days=1)
+    # 日常开发的
+    if type_ == "dev":
+        illegal_c = Controller(branches=["Android-Trunk_DFJZ", "Android-DFJZ_1.2.82.248.1"],    # 检查的分支
+                               excel_name="提交规范检查.xlsx",                                    # excel名
+                               dt_range=generator.illegal_dt_range(),                            # 检查的时间段
+                               scanner=LegalityScanner)
+        illegal_c.run(title="提交规范检查", content="以下提交疑似为不规范提交")
 
-    all_branches = ["Android-Trunk_DFJZ", "Android-DFJZ_1.2.82.248.1"]
+    # 封板阶段的
+    elif type_ == "cbt":
+        online_c = Controller(branches=["Android-DFJZ_1.2.82.248.1"],  # 检查的分支
+                              excel_name="提交检查.xlsx",  # excel名
+                              dt_range=generator.online_dt_range(),  # 检查的时间段
+                              scanner=OnlineScanner)
+        online_c.run(title="提交规范检查", content="以下提交疑似为不规范提交")
 
-    c = Controller()
-    c.run()
-
-    card_msg = bot.send()
-
-    send_with_webhook(card=card_msg, url=url)
-
-    # params = {
-    #     "start": "{2023-04-23}",
-    #     "end": "{2023-04-24}",
-    #     "branch": "Android-Trunk_DFJZ"
-    # }
-    # s = Scanner(**params)
-    # s.scan()
+    # 最后一定是保存的
+    with open("G.json", "w") as f:
+        global user_info
+        json.dump(user_info, f)
---
+++ scan_log/scanner.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+# @Author  : Fang Haoyu
+# @Time    : 2023/5/6 10:41
+# @File    : scanner.py 
+# @Desc    : 扫描svn的操作
+
+import subprocess
+import re
+
+import pandas as pd
+
+
+class Scanner(object):
+    def __init__(self, branch, start, end):
+        self.start = self._format_date(start)
+        self.end = self._format_date(end)
+        self.branch = branch
+
+    def _log_from_date_range(self):
+        """
+        获取指定时间段内的提交日志
+        :return: List[str]
+        """
+        url = f"https://192.168.40.221:8833/svn/mlproj2017/branches/{self.branch}"
+        cmd = f"svn log -r {self.start}:{self.end} {url}"
+        cmd_return = subprocess.run(cmd, encoding="utf-8", stdout=subprocess.PIPE, shell=True)
+        return cmd_return.stdout.split("------------------------------------------------------------------------")
+
+    @staticmethod
+    def _get_commit_time(time_):
+        """
+        格式化提交时间
+        :param time_: svn log中显示的提交时间
+        :return:
+        """
+        pos = time_.find("+")
+        return f"# {time_[:pos-1]}"
+
+    @staticmethod
+    def _log_inline(log):
+        """所有的日志全部显示在一行"""
+        return log.replace("\n", "")
+
+    @staticmethod
+    def _format_date(dt):
+        """将dt格式化为SVN log所需要的形式"""
+        return dt.strftime('{"%Y-%m-%d %H:%M:%S +0800"}')
+
+
+class LegalityScanner(Scanner):
+
+    def scan(self):
+        logs = self._log_from_date_range()
+        illegal = self._scan(logs[1:-1])
+
+        # 如果都是合规提交
+        if not illegal:
+            return
+
+        df = pd.DataFrame(illegal, columns=["提交人", "提交时间", "提交日志"])
+        df["提交分支"] = self.branch
+        return df
+    
+    def _scan(self, logs):
+        illegal = list()
+        for log in logs:
+            if not log:
+                continue
+
+            # 提交信息相关
+            info = re.findall(r".*\|.*\|.*\|.*", log)[0]
+            _, committer, time_, _ = info.split("|")
+            author = committer.strip()
+
+            # 找不到单号且提交人不是builder
+            if not self._is_with_url(log) and author != "builder":
+                log_ = self._log_inline(log.replace(info, ""))
+                # user_name, user_email = self._get_author_name(author)
+                illegal.append([author, self._get_commit_time(time_), log_])
+        return illegal
+
+    @staticmethod
+    def _is_with_url(log):
+        """提交中是否关联了飞书单"""
+        pattern = re.compile(r'.*project\.feishu\.cn.*')
+        return True if re.findall(pattern, log) else False
+
+
+class OnlineScanner(Scanner):
+    """外放分支扫描的"""
+
+    def scan(self):
+        logs = self._log_from_date_range()
+        illegal = self._scan(logs[1:-1])
+
+        # 如果都是合规提交
+        if not illegal:
+            return
+
+        df = pd.DataFrame(illegal, columns=["提交人", "提交时间", "注意项", "提交日志"])
+        df["提交分支"] = self.branch
+        return df
+
+    def _scan(self, logs):
+        illegal = list()
+        for log in logs:
+            if not log:
+                continue
+
+            # 提交信息相关
+            info = re.findall(r".*\|.*\|.*\|.*", log)[0]
+            _, committer, time_, _ = info.split("|")
+            author = committer.strip()
+
+            # builder的提交先不管
+            if author == "builder":
+                continue
+
+            # 判断是否是bug单提交
+            error_reason = self._is_bug_issue(log)
+            if error_reason is None:
+                continue
+
+            log_ = self._log_inline(log.replace(info, ""))
+            illegal.append([author, self._get_commit_time(time_), error_reason, log_])
+        return illegal
+
+    @staticmethod
+    def _is_bug_issue(log):
+        try:
+            url = re.findall(r"project\.feishu\.cn.*/.*/.*/\d+", log)[0]
+        except IndexError:
+            return "提交日志中无[飞书项目]单子信息"
+        else:
+            split_ = url.split("/")
+            issue_type = split_[-3]
+
+            if issue_type != "issue":
+                return "提交的单子不是Bug单！"
+
+
+
